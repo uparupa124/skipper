1f3ee743bd6b1edb9088be6377a0311c4b4e6f6b
EC2 Default User <ec2-user@ip-172-31-6-217.ap-northeast-1.compute.internal>
2021-05-28 06:53:45 +0000

    [finish]jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,17 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+
+$(document).ready(function () {
+  $('.jquery').on('click', function(){
+    $(this).css('color','red');
+  });
+});
+
